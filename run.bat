28824b2c7eee29c3b7012085f74f662de7dc6e7b
Daniel Amayaenvbo <159960133+doa29@users.noreply.github.com>
2025-05-19 14:30:50 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,15 @@
 @echo off
+REM Create venv if it doesn't exist
 IF NOT EXIST venv (
     python -m venv venv
 )
+
+REM Activate the virtual environment
 CALL venv\Scripts\activate
+
+REM Upgrade pip and install requirements
+python -m pip install --upgrade pip
 pip install -r requirements.txt
-python downloader.py %*
+
+REM Run the Streamlit app
+streamlit run downloader.py %*
